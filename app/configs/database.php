d5018f150d478c21990ce3ab5279de66e13eb5e3
ql <3094047@qq.com>
2015-03-13 18:41:05 +0800

    init

---
+++ app/configs/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  数据库配置设置 
+ */
+
+return array(
+	'fetch' => PDO::FETCH_CLASS,
+		
+	// 默认使用的数据库连接，对应 connections数组里的key
+	'default' => 'master',
+	
+	'connections' => array(
+		'master' => array(
+			'driver'    => 'mysql',
+			'host'      => '127.0.0.1',
+			'database'  => 'qsmvc',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+	),
+	
+);
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,2 +1,9 @@
 <?php
 	
+	// 路径设置
+	$paths = array(
+		'app' => __DIR__.'/../app',
+		'public' => __DIR__.'/../public',
+		'base' => __DIR__.'/..',
+		'storage' => __DIR__.'/../app/storage',
+	);
